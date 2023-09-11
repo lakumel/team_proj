0e4f830f8180becb65a585a12eed82b19c2cb14e
Yang woosung <ngxk1217@gmail.com>
2023-09-11 11:16:06 +0900

    프로젝트

---
+++ src/test/java/com/mysite/hi/TeamProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.hi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
